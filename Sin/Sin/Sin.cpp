eb50268772f63cfb91127d615e96ea7e05de6583
VadimUrazov <vadim.urazov@gmail.com>
2020-12-29 23:22:21 +0600

    Добавьте файлы проекта.

---
+++ Sin/Sin/Sin.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#define PI 3.141592653589793238462643383279
+
+using namespace std;
+double Sin(double x, double eps = 1e-5)
+{
+    double S = 0.;
+    double N = 1.;
+    double D = 2.;
+    double edm = -1.;
+    double ed = 1.;
+
+    double pi2 = PI * 2;
+    x = fmod(x, pi2);
+    double P = x;
+
+    double R;
+    int sw;
+    _asm {
+        finit
+        fld x
+        fstp S
+        FOR : fld N
+        fmul D
+        fst R
+        fmul R
+        fadd R
+        fst R
+        fld x
+        fld x
+        fmul
+        fld edm
+        fmul
+
+        fdiv R
+        fmul P
+        fst P
+        fadd S
+        fstp S
+        fld N
+        fadd ed
+        fstp N
+        finit
+        fld P
+        fabs
+        fld eps
+
+        fcomi st(0), st(1)
+        jna FOR
+    }
+    return S;
+}
+
+int main()
+{
+    cout << "teaylor:" << Sin(1000000000) << endl;
+    cout << "math sin:" << sin(1000000000) << endl;
+    cout << "error:" << 1 - Sin(1000000000) / sin(1000000000) << endl;
+    cout << endl;
+
+    cout << "teaylor:" << Sin(DBL_MIN) << endl;
+    cout << "math sin:" << sin(DBL_MIN) << endl;
+    cout << "error:" << 1 - Sin(DBL_MIN) / sin(DBL_MIN) << endl;
+    cout << endl;
+
+    cout << "teaylor:" << Sin(1.0) << endl;
+    cout << "math sin:" << sin(1.0) << endl;
+    cout << "error:" << 1 - Sin(1.0) / sin(1.0) << endl;
+    cout << endl;
+
+    cout << "teaylor:" << Sin(PI / 6) << endl;
+    cout << "math sin:" << sin(PI / 6) << endl;
+    cout << "error:" << 1 - Sin(PI / 6) / sin(PI / 6) << endl;
+    cout << endl;
+
+    cout << "teaylor:" << Sin(PI / 4) << endl;
+    cout << "math sin:" << sin(PI / 4) << endl;
+    cout << "error:" << 1 - Sin(PI / 4) / sin(PI / 4) << endl;
+    cout << endl;
+
+    cout << "teaylor:" << Sin(0.0) << endl;
+    cout << "math sin:" << sin(0.0) << endl;
+    cout << "error:" << 0<< endl;
+    cout << endl;
+    
+    return 0;
+}
+/*
+Sin(1000000000)
+teaylor:0.545843
+ideal:0.545843
+error:-2.39346e-08
+
+Sin(DBL_MIN)
+teaylor:2.22507e-308
+ideal:2.22507e-308
+error:0
+
+Sin(1.0)
+teaylor:0.841471
+ideal:0.841471
+error:-2.95819e-08
+
+Sin(PI/6)
+teaylor:0.5
+ideal:0.5
+error:1.6262e-08
+
+Sin(PI/4)
+teaylor:0.707107
+ideal:0.707107
+error:-2.47533e-09
+
+Sin(0.0)
+teaylor:0
+ideal:0
+error:0
+
+*/
---
+++ substring/substring/substring.cpp
@@ -0,0 +1,143 @@
+﻿
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+int substr() {
+    char input_s[] = "input string:\n";
+    char input_p[] = "input position:\n";
+    char result[] = "result:\n%s";
+    char incorrect[] = "incorrect position!\n";
+    char d[] = "%d";
+    char s[] = "%s";
+
+    char str[512];
+    char* res;
+    int pos = 0;
+    __asm {
+        ; printf("input string:\n");
+        lea     eax, input_s
+            push    eax
+            call    printf
+            add     esp, 4
+
+            ; gets_s(str, 512)
+            lea     ebx, str
+            push    512
+            push    ebx
+            call    gets_s
+            add     esp, 8
+
+            ; printf("input position:\n");
+        lea     eax, input_p
+            push    eax
+            call    printf
+            add     esp, 4
+
+            ; scanf("%d", &pos)
+            lea     eax, pos
+            push    eax
+            lea     eax, d
+            push    eax
+            call    scanf
+            add     esp, 8
+
+            ; Определим длину строки
+            ; strlen(str)
+            lea     eax, str
+            push    eax
+            call    strlen
+            add     esp, 4
+
+            ; Распределим данные по регистрам :
+        ; ebx - str
+            ; ecx - n
+            ; edx - newSize
+            ; esi - i
+            ; edi - j
+            ; dl и eax - буферный элемент
+            ;Найдём длину новой строки
+            ; n = strlen(str)
+            mov     ecx, eax
+            mov     edx, eax
+            lea     ebx, str
+
+            ; newSize = n - pos;
+        mov     eax, [pos]
+            sub     edx, eax
+
+            ; if (newSize < 1 || newSize > n)
+            mov     eax, 1
+            cmp     edx, eax
+            jl      ERR
+            cmp     edx, ecx
+            jng     MLC
+
+            ERR :
+        ; printf("incorrect position!");
+        lea     eax, incorrect
+            push    eax
+            call    printf
+            add     esp, 4
+            jmp     FIN
+
+            MLC :
+        ; сохраняем регистры в стек
+            push    edx
+            push    ecx
+
+            ; malloc(newSize + 1)
+            inc     edx
+            push    edx
+            call    malloc
+            add     esp, 4
+            ; выделенная память сохранилась в eax
+            ; восстанавливаем n из стека
+            pop     ecx
+
+            ; инициализация цикла
+            lea     ebx, str
+            mov     esi, pos
+            xor edi, edi
+            FOR :
+        ; i < n
+            cmp     esi, ecx
+            je      NXT
+            mov     dl, [ebx][esi]
+            mov[eax][edi], dl
+            inc     esi
+            inc     edi
+            jmp     FOR
+            NXT :
+        pop     edx
+            mov[eax][edx], '\0'
+
+            ; printf("result:\n%s", res);
+        push    eax
+            lea     eax, result
+            push    eax
+            call    printf
+            add     esp, 8
+            FIN:
+    }
+}
+int main() {
+    //int but = 1;
+    //if (but == 1) {
+
+      //  while (but != 2) {
+            substr();
+        //    cout << endl;
+          //  printf("enter button");
+           // cout << endl;
+            //cin >> but;
+            //cout << endl;
+       // }
+    //}
+    //else {
+      //  return 0;
+    //}
+    return 0;
+}
